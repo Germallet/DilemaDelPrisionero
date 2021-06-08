7df92c2ff509dd96ba209e7249f2b6f504b7c5b1
Germán Mallet <germanmallet@gmail.com>
2021-06-07 21:08:52 -0300

    Project

---
+++ DilemaPrisionero/Content/Effects/DilemaShader.fx
@@ -0,0 +1,109 @@
+﻿#if OPENGL
+	#define SV_POSITION POSITION
+	#define VS_SHADERMODEL vs_3_0
+	#define PS_SHADERMODEL ps_3_0
+#else
+	#define VS_SHADERMODEL vs_4_0_level_9_1
+	#define PS_SHADERMODEL ps_4_0_level_9_1
+#endif
+
+static const float width = 1024;
+static const float height = 786;
+static const float dx = 1 / width;
+static const float dy = 1 / height;
+
+static const float T = 2.7;
+static const float R = 1.2;
+static const float C = 0.1;
+static const float PointsMatrix[2][2] = { { R, 0 }, { T, C } };
+static const float2 OffsetsMatrix[8] = { float2(-dx, dy),  float2(0, dy),  float2(dx, dy),
+										 float2(-dx, 0),                   float2(dx, 0),
+										 float2(-dx, -dy), float2(0, -dy), float2(dx, -dy) };
+
+texture inputTexture;
+sampler2D textureSampler = sampler_state
+{
+	Texture = (inputTexture);
+	MagFilter = None;
+	MinFilter = None;
+	AddressU = Wrap;
+	AddressV = Wrap;
+};
+
+struct VertexShaderInput
+{
+	float4 Position : POSITION0;
+    float2 TextureCoordinate : TEXCOORD0;
+};
+
+struct VertexShaderOutput
+{
+	float4 Position : SV_POSITION;
+    float2 TextureCoordinate : TEXCOORD0;
+};
+
+VertexShaderOutput MainVS(in VertexShaderInput input)
+{
+	VertexShaderOutput output = (VertexShaderOutput)0;
+    output.Position = input.Position;	
+    output.TextureCoordinate = input.TextureCoordinate;
+    return output;
+}
+
+float GetStrategy(float2 pos)
+{
+	return tex2D(textureSampler, pos).x;
+}
+
+float4 CalculatePointsPS(VertexShaderOutput input) : COLOR
+{
+	float selfStrategy = GetStrategy(input.TextureCoordinate);
+
+	float totalPoints = 0;
+	for (int i = 0; i < 8; i++)
+		totalPoints += PointsMatrix[selfStrategy][GetStrategy(input.TextureCoordinate + OffsetsMatrix[i])];
+
+	return float4(selfStrategy, 1/totalPoints, 0, 1);
+}
+
+float GetPoints(float2 pos)
+{
+	return 1/tex2D(textureSampler, pos).y;
+}
+
+float4 ChooseStrategyPS(VertexShaderOutput input) : COLOR
+{
+	float maxPoints = GetPoints(input.TextureCoordinate);
+	float2 bestOffset = float2(0, 0);
+
+	float candidatePoints;
+	for (int i = 0; i < 8; i++)
+	{
+		candidatePoints = GetPoints(input.TextureCoordinate + OffsetsMatrix[i]);
+		if (candidatePoints > maxPoints)
+		{
+			bestOffset = OffsetsMatrix[i];
+			maxPoints = candidatePoints;
+		}
+	}
+
+	return float4(GetStrategy(input.TextureCoordinate + bestOffset), 0, 0, 1);
+}
+
+technique CalculatePoints
+{
+	pass P0
+	{
+		VertexShader = compile VS_SHADERMODEL MainVS();
+		PixelShader = compile PS_SHADERMODEL CalculatePointsPS();
+	}
+};
+
+technique ChooseStrategy
+{
+	pass P0
+	{
+		VertexShader = compile VS_SHADERMODEL MainVS();
+		PixelShader = compile PS_SHADERMODEL ChooseStrategyPS();
+	}
+};
---
+++ DilemaPrisionero/Sources/Dilema.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace DilemaPrisionero
+{
+    public static class Dilema
+    {
+        [STAThread]
+        static void Main()
+        {
+            using var game = new TGCGame();
+            game.Run();
+        }
+    }
+
+    internal class TGCGame : Game
+    {
+        private SpriteBatch spriteBatch;
+        private Effect dilemaShader;
+        private FullScreenQuad fullScreenQuad;
+        private RenderTarget2D renderInput, renderPoints, renderTarget;
+        private const int width = 1024, height = 786;
+
+        internal TGCGame() => new GraphicsDeviceManager(this);
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+            Window.Title = "Dilema del Prisionero";
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            dilemaShader = Content.Load<Effect>("Content/Effects/DilemaShader");
+            fullScreenQuad = new FullScreenQuad(GraphicsDevice);
+            renderInput = new RenderTarget2D(GraphicsDevice, width, height, false, SurfaceFormat.Color, DepthFormat.None);
+            renderPoints = new RenderTarget2D(GraphicsDevice, width, height, false, SurfaceFormat.Vector2, DepthFormat.None);
+            renderTarget = new RenderTarget2D(GraphicsDevice, width, height, false, SurfaceFormat.Color, DepthFormat.None);
+            renderInput.SetData(InitialCenter());   
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.SetRenderTarget(renderPoints);
+            dilemaShader.CurrentTechnique = dilemaShader.Techniques["CalculatePoints"];
+            dilemaShader.Parameters["inputTexture"].SetValue(renderInput);
+            fullScreenQuad.Draw(dilemaShader);
+
+            GraphicsDevice.SetRenderTarget(renderTarget);
+            dilemaShader.CurrentTechnique = dilemaShader.Techniques["ChooseStrategy"];
+            dilemaShader.Parameters["inputTexture"].SetValue(renderPoints);
+            fullScreenQuad.Draw(dilemaShader);
+
+            GraphicsDevice.SetRenderTarget(null);
+            spriteBatch.Begin();
+            spriteBatch.Draw(renderTarget, new Rectangle(0, 0, Window.ClientBounds.Width, Window.ClientBounds.Height), Color.White);
+            spriteBatch.End();
+
+            RenderTarget2D renderSwap = renderInput;
+            renderInput = renderTarget;
+            renderTarget = renderSwap;
+        }
+
+        private Color[] InitialCenter()
+        {
+            Color[] colors = new Color[width * height];
+            colors[width * height / 2 - width / 2] = new Color(255, 0, 0, 0);
+            return colors;
+        }
+
+        protected override void UnloadContent()
+        {
+            Content.Unload();
+            fullScreenQuad.Dispose();
+        }
+    }
+
+    internal class FullScreenQuad
+    {
+        private readonly GraphicsDevice device;
+        private IndexBuffer indexBuffer;
+        private VertexBuffer vertexBuffer;
+
+        internal FullScreenQuad(GraphicsDevice device)
+        {
+            this.device = device;
+            CreateVertexBuffer();
+            CreateIndexBuffer();
+        }
+
+        private void CreateVertexBuffer()
+        {
+            var vertices = new VertexPositionTexture[4];
+            vertices[0].Position = new Vector3(-1f, -1f, 0f);
+            vertices[0].TextureCoordinate = new Vector2(0f, 1f);
+            vertices[1].Position = new Vector3(-1f, 1f, 0f);
+            vertices[1].TextureCoordinate = new Vector2(0f, 0f);
+            vertices[2].Position = new Vector3(1f, -1f, 0f);
+            vertices[2].TextureCoordinate = new Vector2(1f, 1f);
+            vertices[3].Position = new Vector3(1f, 1f, 0f);
+            vertices[3].TextureCoordinate = new Vector2(1f, 0f);
+
+            vertexBuffer = new VertexBuffer(device, VertexPositionTexture.VertexDeclaration, 4, BufferUsage.WriteOnly);
+            vertexBuffer.SetData(vertices);
+        }
+
+        private void CreateIndexBuffer()
+        {
+            ushort[] indices = new ushort[6] { 0, 1, 3, 0, 3, 2 };
+            indexBuffer = new IndexBuffer(device, IndexElementSize.SixteenBits, 6, BufferUsage.WriteOnly);
+            indexBuffer.SetData(indices);
+        }
+
+        internal void Draw(Effect effect)
+        {
+            device.SetVertexBuffer(vertexBuffer);
+            device.Indices = indexBuffer;
+
+            foreach (var pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                device.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, 2);
+            }
+        }
+
+        internal void Dispose()
+        {
+            vertexBuffer.Dispose();
+            indexBuffer.Dispose();
+        }
+    }
+}
